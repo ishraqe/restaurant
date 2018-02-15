7105a5fa47b514534ca1aee2522daea600ceb646
ishraqe <kanak431@diu.edu.bd>
2018-02-15 17:11:36 +0600

    bookmark page

--- src/screen/Bookmark.js
+++ src/screen/Bookmark.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import ListComponent from '../component/Search/ListComponent';
 
 class Bookmark extends Component {
     render() {
         return (
-            <View><Text>Bookmark</Text></View>
+            <View style={{flex:1}}>
+                <View style={{ width: '100%',}}>
+                    <FlatList
+                        data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }, { key: 'e' }, { key: 'f' }, { key: 'g' }, { key: 'h' }, { key: 'i' }, { key: 'j' }, { key: 'k' }, { key: 'l' }, { key: 'm' }, { key: 'n' }, { key: 'o' }, { key: 'p' }, { key: 'q' }, { key: 'r' }, { key: 's' }, { key: 't' }, { key: 'u' }, { key: 'v' }, { key: 'w' }, { key: 'x' }, { key: 'y' }, { key: 'z' }, { key: '1' }, { key: '2' }, { key: '3' }, { key: '4' }, { key: '5' }, { key: '6' }, { key: '7' }, { key: '8' }, { key: '9' }, { key: '10' }, { key: '11' }, { key: '12' }, { key: '13' }, { key: '14' }, { key: '15' }, { key: '16' }]}
+                        renderItem={({ item }) => (
+                            <ListComponent
+                                item={item}
+                            />
+                        )}
+                    />
+
+                </View>
+            </View>
         );
     }
 }
